fe2b1e7b6bf5f9b5b7168356666a761500f20de9
Aaron Papp <Aaron-P@users.noreply.github.com>
2021-10-25 11:43:53 +0000

    Limit split, replace content instead of appending

--- internationalization.js
+++ internationalization.js
@@ -1,7 +1,7 @@
 // ----------------- Internationalization ------------------
 document.querySelectorAll('[data-i18n]').forEach(node => {
-  let [text, attr] = node.dataset.i18n.split('|');
+  let [text, attr] = node.dataset.i18n.split('|', 2);
   text = chrome.i18n.getMessage(text);
-  attr ? node.setAttribute(attr, text) : node.append(text);
+  attr ? node.setAttribute(attr, text) : node.textContent = text;
 });
 // ----------------- /Internationalization -----------------
